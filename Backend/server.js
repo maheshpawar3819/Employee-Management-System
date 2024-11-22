f75bb5ef147b860e0f171028d1e074c302f30f11
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-11-22 14:25:06 +0530

    setup backend for project

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express=require("express");
+const app=express();
+const cors=require("cors");
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const port=process.env.PORT
+app.listen(port,() => {
+    console.log(`Server is listning on port`,port)
+})
