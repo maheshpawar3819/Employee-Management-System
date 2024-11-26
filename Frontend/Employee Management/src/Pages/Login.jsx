d9beb22895442f0a72ade9df6704b97b566e999d
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-11-26 15:22:03 +0530

    add styling to login page

--- Frontend/Employee Management/src/Pages/Login.jsx
+++ Frontend/Employee Management/src/Pages/Login.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleSubmit = async(e) => { 
+    e.preventDefault();
+    try {
+        const response=await axios.post("http://localhost:8080/api/auth/login",{
+            email,password
+        })
+
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    } 
+  };
+
+  return (
+    <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6">
+      <h2 className="font-pacifico text-3xl text-white">
+        Employee Management System
+      </h2>
+      <div className="border shadow p-6 w-80 bg-white">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="w-full px-3 py-2 border"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your passowrd"
+              className="w-full px-3 py-2 border"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mb-4 flex items-center justify-between">
+            <label htmlFor="" className="inline-flex items-center">
+              <input type="checkbox" className="form-checkbox" />
+              <span className="ml-2 text-gray-700">Remember me</span>
+            </label>
+            <a href="#" className="text-teal-600">
+              Forgot Password?
+            </a>
+          </div>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="w-full bg-teal-600 text-white py-2"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
