e369fb8df360b135f6bf51138008d918a3432ba1
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-11-26 14:43:45 +0530

    setup routes for frontend

--- Frontend/Employee Management/src/App.jsx
+++ Frontend/Employee Management/src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Pages/Login";
+import AdminDashboard from "./Pages/AdminDashboard";
 
 function App() {
-
   return (
-    <>
-      <p className='text-'>this is home page</p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={"/admin-dashboard"} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+ 
+export default App;
